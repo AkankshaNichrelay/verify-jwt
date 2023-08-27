b749639258b1574e2dacf4fadc94f6748911256c
AkankshaNichrelay <kanksha.nichrelay@gmail.com>
2023-08-27 17:27:48 -0500

    Adding basic skeleton endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpServer, Responder, HttpResponse};
+use serde::{Deserialize};
+
+#[derive(Debug, Deserialize)]
+struct MyToken {
+    jwt: String,
+    jwk: String,
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello, World!"
+}
+
+#[post("/verify-jwt")]
+async fn verify_jwt(mytokens: web::Json<MyToken>) -> impl Responder {
+    println!("jwt: {:?}", mytokens.jwt);
+    println!("jwk: {:?}", mytokens.jwk);
+    HttpResponse::Ok().body("Received tokens.")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index).service(verify_jwt))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
